c2ec6cb6204481302f93b3c9ffbb59efaa97da38
sasobapriyanjana11 <sasoba.zxc@gmail.com>
2023-12-18 11:07:05 +0530

    use less boilerplate rules to Customer Form

---
+++ layered-architecture/src/main/java/com/example/layeredarchitecture/dao/sqlUtil.java
@@ -0,0 +1,31 @@
+package com.example.layeredarchitecture.dao;
+
+import com.example.layeredarchitecture.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class sqlUtil {
+    public static <T> T execute(String sql,Object... args) throws SQLException, ClassNotFoundException {
+        Connection connection = DBConnection.getDbConnection().getConnection();
+
+       /* PreparedStatement pstm = connection.prepareStatement("UPDATE Customer SET name=?, address=? WHERE id=?");
+        pstm.setString(1, dto.getName());
+        pstm.setString(2, dto.getAddress());
+        pstm.setString(3, dto.getId());
+        pstm.executeUpdate();*/
+
+        PreparedStatement pstm=connection.prepareStatement(sql);
+
+        for(int i=0 ; i < args.length;i++){
+            pstm.setObject(i+1,args[i]);
+        }
+        if(sql.startsWith("SELECT")){
+            return (T) pstm.executeQuery();
+        }else{
+           return (T) (Boolean) (pstm.executeUpdate()>0);
+        }
+
+    }
+}
